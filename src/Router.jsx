17a0ffe0f52a8afbb2134e6c72e4dd34e6befe38
Daniil Palagin <daniilpalaginjob@gmail.com>
2024-11-05 19:04:19 +0100

    [#78] Use Layout Router to render layouts for each component.

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import { Routes, BrowserRouter, Route } from "react-router-dom";
-import Layout from "./components/Layout.jsx";
 import Home from "./components/Home.jsx";
-import Scripts from "./components/Scripts.jsx";
 import Executions from "./components/Executions.jsx";
-import NoMatch from "./components/NoMatch.jsx";
+import Scripts from "./components/Scripts.jsx";
 import Dagre from "./components/dagre/Dagre.jsx";
+import NoMatch from "./components/NoMatch.jsx";
+import Layout from "./components/Layout.jsx";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="scripts" element={<Scripts />} />
-        <Route path="executions" element={<Executions />} />
-        <Route path="*" element={<NoMatch />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="scripts" element={<Scripts />} />
+          <Route path="executions" element={<Executions />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
         <Route path="script" element={<Dagre />} />
       </Routes>
     </BrowserRouter>
--- src/components/NoMatch.jsx
+++ src/components/NoMatch.jsx
@@ -5,10 +5,10 @@ import Layout from "./Layout";
 
 const NoMatch = () => {
   return (
-    <Layout>
+    <>
       <Icon name="minus circle" size="big" />
       <strong>Page not found!</strong>
-    </Layout>
+    </>
   );
 };
 
