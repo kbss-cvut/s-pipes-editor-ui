cc9d5fe96077ec7a14b69af6c4063f0e21f89440
Daniil Palagin <daniilpalaginjob@gmail.com>
2025-02-14 09:52:35 +0100

    Support configuration of s-pipes-editor in docker deployment

--- src/config/env.js
+++ src/config/env.js
@@ -1,44 +1,17 @@
-/**
- * Prefix used for Vite environment variables.
- * @constant {string}
- */
-const VITE_ENV_PREFIX = "S_PIPES_";
+const getEnv = (name, defaultValue) => {
+  let ENV;
+  if (window.__config__ && Object.keys(window.__config__).length > 0) {
+    ENV = {
+      ...window.__config__,
+    };
+  } else ENV = import.meta.env;
 
-/**
- * Object containing environment variables.
- * - Includes environment variables from `import.meta.env` that start with `VITE_ENV_PREFIX`.
- * - Merges in configuration from `window.__config__`.
- * @type {Object<string, string>}
- */
-const ENV = {
-  ...Object.keys(import.meta.env).reduce((accumulator, key) => {
-    if (key.startsWith(VITE_ENV_PREFIX)) {
-      const strippedKey = key.replace(VITE_ENV_PREFIX, "");
-      accumulator[strippedKey] = import.meta.env[key];
-    }
-    return accumulator;
-  }, {}),
-  ...window.__config__,
-};
-
-/**
- * Retrieves the value of an environment variable.
- * - Returns the value if it exists, otherwise returns the provided default value.
- * - Throws an error if the variable is not found and no default value is provided.
- *
- * @param {string} name - The name of the environment variable to retrieve.
- * @param {string} [defaultValue] - Optional default value to return if the environment variable is not found.
- * @returns {string} The value of the environment variable or the default value.
- * @throws {Error} If the environment variable is not found and no default value is provided.
- */
-export const getEnv = (name, defaultValue) => {
   const value = ENV[name] || defaultValue;
-
   if (value !== undefined) {
     return value;
   }
-
   throw new Error(`Missing environment variable: ${name}`);
 };
 
-export const APP_TITLE = getEnv("APP_TITLE", "SPipes Editor");
+export const APP_TITLE = getEnv("S_PIPES_EDITOR_APP_TITLE", "SPipes Editor");
+export const API_URL = getEnv("S_PIPES_API_EDITOR_URL", "http://localhost:1235/rest");
--- src/rest/api.js
+++ src/rest/api.js
@@ -1,5 +1,8 @@
 import axios from "axios";
+import { API_URL } from "../config/env.js";
 
-const api = axios.create();
+const api = axios.create({
+  baseURL: API_URL,
+});
 
 export default api;
