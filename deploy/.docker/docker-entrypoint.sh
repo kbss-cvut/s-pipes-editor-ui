8e24ad00ba10f629832c8f3c86051d01dd5a6f14
Daniil Palagin <daniilpalaginjob@gmail.com>
2025-02-14 09:52:35 +0100

    Support configuration of s-pipes-editor in docker deployment

--- deploy/.docker/docker-entrypoint.sh
+++ deploy/.docker/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst '${SERVICE_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${S_PIPES_APP_TITLE} ${S_PIPES_API_URL} ' < /etc/nginx/config.js.template > /usr/share/nginx/html/config.js
 
 exec "$@"
