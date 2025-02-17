6c02e569ef3b4dbc0ab442d85d0a68725688bfd0
Daniil Palagin <daniilpalaginjob@gmail.com>
2025-02-17 09:42:35 +0100

    Update DelayRender time for better user experience

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,6 +1,6 @@
 import Spinner from "./spinner/Spinner";
 import { DelayRender } from "./DelayRender";
 
-const Loading = DelayRender(Spinner, 0);
+const Loading = DelayRender(Spinner, 300);
 
 export default Loading;
