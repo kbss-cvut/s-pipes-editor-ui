814b8570e2a257bec3b4d34331e35d98b50258e4
palagdan <daniilpalaginjob@gmail.com>
2025-09-04 15:37:01 +0200

    Refactor ErrorModal to functional component. Remove the deprecated componentWillReceiveProps function

--- src/components/modal/ErrorModal.tsx
+++ src/components/modal/ErrorModal.tsx
@@ -1,79 +1,55 @@
-import React from "react";
+import React, { Dispatch, SetStateAction, useState } from "react";
 
 import { Button, Modal } from "react-bootstrap";
 
-class ErrorModal extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isLoaded: false,
-      modalVisible: false,
-      errorMessage: null,
-      subscript: null,
-    };
-
-    this.handleClose = this.handleClose.bind(this);
-  }
-
-  componentWillReceiveProps(newProps) {
-    if (newProps.errorMessage) {
-      this.setState({
-        isLoaded: true,
-        modalVisible: true,
-        errorMessage: newProps["errorMessage"],
-        subscript: newProps["subscript"],
-      });
-    }
-  }
-
-  handleClose() {
-    this.props.handleErrorModal();
-    this.setState({ errorMessage: null, subscript: null });
-  }
-
-  render() {
-    if (this.state.errorMessage) {
-      const subscript = this.state.subscript;
-      return (
-        <Modal
-          show={true}
-          onHide={() => this.handleClose()}
-          dialogClassName="modal-80w"
-          aria-labelledby="example-custom-modal-styling-title"
-        >
-          <Modal.Header closeButton>
-            <Modal.Title>Error</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            {this.state.errorMessage.split("\n").map((line, index) => (
-              <React.Fragment key={index}>
-                {line}
-                <br />
-              </React.Fragment>
-            ))}
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={() => this.handleClose()}>
-              Close
-            </Button>
-            <Button
-              variant="primary"
-              onClick={() => {
-                const url = `${window.location.origin}/script?file=${subscript}`;
-                console.log("Opening subscript editor at:", subscript);
-                window.open(url, "_blank");
-              }}
-            >
-              Open SubScript in Editor
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      );
-    } else {
-      return null;
-    }
-  }
+interface ErrorModalProps {
+  errorMessage: string;
+  handleErrorModal: () => void;
+  subscript?: string;
 }
+
+const ErrorModal: React.FC<ErrorModalProps> = ({ errorMessage, handleErrorModal, subscript }) => {
+  const handleClose = () => {
+    handleErrorModal();
+  };
+
+  if (errorMessage == null) return null;
+
+  return (
+    <Modal
+      show={true}
+      onHide={() => handleClose()}
+      dialogClassName="modal-80w"
+      aria-labelledby="example-custom-modal-styling-title"
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>Error</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {errorMessage.split("\n").map((line, index) => (
+          <React.Fragment key={index}>
+            {line}
+            <br />
+          </React.Fragment>
+        ))}
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={() => handleClose()}>
+          Close
+        </Button>
+        <Button
+          variant="primary"
+          onClick={() => {
+            const url = `${window.location.origin}/script?file=${subscript}`;
+            console.log("Opening subscript editor at:", subscript);
+            window.open(url, "_blank");
+          }}
+        >
+          Open SubScript in Editor
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
 
 export default ErrorModal;
